d6993608aecdc9267d52331901b2576e99eaf3d7
Patrick Paul N. Leonen <leonenpatrickpaul@gmail.com>
2023-06-10 12:15:28 -0700

    Activity 5 of Paul Leonen

--- Leonen_PatrickPaul/Activity 5/index.php
+++ Leonen_PatrickPaul/Activity 5/index.php
@@ -5,18 +5,29 @@ $item3 = 300;
 
 function getRemainingStocksOfItem1 ($item, $qtybought, $item1) {
 $item1 -= $qtybought;
-return "$item - ($qtybought) qty | Remaining Stock = $item1";
-$item1 -= $qtybought;
+return "$item - ($qtybought) qty | Remaining Stocks = $item1";
 }
 
 function getRemainingStocksOfItem2 ($item, $qtybought, $item2) {
-$remainingstock = $item2 - $qtybought;
-return "$item - ($qtybought) qty | Remaining Stock = $remainingstock";
+$item2 -= $qtybought;
+return "$item - ($qtybought) qty | Remaining Stocks = $item2";
 }
 
-echo getRemainingStocksOfItem1 ("Item 1", 3, $item1);
+function getRemainingStocksOfItem3 ($item, $qtybought, $item3) {
+$item3 -= $qtybought;
+return "$item - ($qtybought) qty | Remaining Stocks = $item3";
+}
+
+
+echo getRemainingStocksOfItem1 ("Item 1", 3, 100);
 echo "\n";
-echo getRemainingStocksOfItem1 ("Item 1", 5, $item1);
+echo getRemainingStocksOfItem1 ("Item 1", 5, 97);
+echo "\n";
+echo getRemainingStocksOfItem2 ("Item 2", 5, 200);
+echo "\n";
+echo getRemainingStocksOfItem1 ("Item 1", 50, 92);
+echo "\n";
+echo getRemainingStocksOfItem3 ("Item 3", 5, 300);
 
 
 
