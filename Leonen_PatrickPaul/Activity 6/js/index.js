72c2724fc20ed3002d113201c85174cdd2ab7cc6
Patrick Paul Leonen <leonenpatrickpaul@gmail.com>
2023-06-25 04:06:22 +0800

    Activity 6 progress Paul Leonen WIP

---
+++ Leonen_PatrickPaul/Activity 6/js/index.js
@@ -0,0 +1,27 @@
+let localUsername = "admin";
+let localPassword = "password";
+
+function login() {
+    var adminInput = document.getElementById("username").value;
+    var passwordInput = document.getElementById("password").value;
+
+    if (adminInput === localUsername && passwordInput === localPassword) {
+        showAlert("success", "Successfully Login");
+        window.location.href = "registration.html";
+    } else if (adminInput == "" | passwordInput === ""){
+        showAlert("warning", "Please Input Username and Password");
+    } else if(adminInput !== localUsername | passwordInput !== localPassword){
+        showAlert("danger", "Invalid Username or Password");
+    }
+}
+
+function showAlert(alertType, message) {
+    var alertContainer = document.createElement("div");
+    alertContainer.classList.add("alert", "alert-" + alertType);
+    alertContainer.innerHTML = message;
+
+    var container = document.querySelector(".container");
+    container.appendChild(alertContainer);
+}
+
+
