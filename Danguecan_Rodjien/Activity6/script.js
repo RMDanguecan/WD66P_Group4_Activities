e1d09a73fc78e6e2ae910888968c399098be1ce7
Rodjien Danguecan <Rodjiendanguecan@gmail.com>
2023-06-24 15:28:18 +0800

    Adding_2more_alerts

--- Danguecan_Rodjien/Activity6/script.js
+++ Danguecan_Rodjien/Activity6/script.js
@@ -1,17 +1,57 @@
-$(document).ready(function() {
-    $('#loginBtn').click(function(event) {
-      event.preventDefault();
-      var username = $('#username').val();
-      var password = $('#password').val();
-  
-      if (username.trim() == '' || password.trim() == '') {
-        $('#errorMessage').html('Please enter both email address and password.').show();
-      } else {
+function closeAlert(alertId) {
+    document.getElementById(alertId).style.display = "none";
+}
+
+function showEmptyFieldAlert() {
+    closeAlert("invalidMessage");
+    closeAlert("successMessage");
+    document.getElementById("errorMessage").style.display = "block";
+}
+
+function showInvalidCredentialsAlert() {
+    closeAlert("errorMessage");
+    closeAlert("successMessage");
+    document.getElementById("invalidMessage").style.display = "block";
+}
+
+function showSuccessAlert() {
+    closeAlert("errorMessage");
+    closeAlert("invalidMessage");
+    document.getElementById("successMessage").style.display = "block";
+}
+
+function validateForm() {
+    closeAlert("errorMessage");
+    closeAlert("invalidMessage");
+    closeAlert("successMessage");
+
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    if (username.trim() == "" || password.trim() == "") {
+        showEmptyFieldAlert();
+        return false;
+    }
+
+    if (username !== "admin" || password !== "password") {
+        showInvalidCredentialsAlert();
+        return false;
+    }
+
+    showSuccessAlert();
+    return false;
+}
+
+document.getElementById("loginBtn").addEventListener("click", function(event) {
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
     
-      }
-    });
-  
-    $('.close').click(function() {
-      $(this).parent().hide();
-    });
-  });
+    if (username.trim() == "" || password.trim() == "") {
+        event.preventDefault();
+        showEmptyFieldAlert();
+    } else {
+        closeAlert("errorMessage");
+        closeAlert("invalidMessage");
+        closeAlert("successMessage");
+    }
+});
